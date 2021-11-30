e4e49617552a85ab3e88f5e41d114b2b1478ff5d
Sandra Krejcir <krejcir.sandra@gmail.com>
2021-11-30 19:04:36 +0100

    component form

--- src/App.svelte
+++ src/App.svelte
@@ -1,23 +1,81 @@
 <script>
   import logo from './assets/svelte.png'
   import Counter from './lib/Counter.svelte'
+  import Payform from './lib/Payform.svelte'
+  let types = ['API', 'GODknows']
+  let beers = ['Heiniken', 'Karlovacko', 'Pan']
 </script>
 
 <main>
-  <img src={logo} alt="Svelte Logo" />
-  <h1>Hello world!</h1>
+<section class="selection">
+  <div>
+    <span></span>
+    <img src="{logo}" alt="The company's logo">
+<span></span>
+<ul class="firstFilter">
+  <li>Full selection</li>
+  <li>On today's tap</li>
+</ul>
+<ul class="secondFilter">
+  <li>All</li>
+  {#each types as type}
+  <li>{type}</li>
+  {/each}
+</ul>
+<h2>Your Order</h2>
+  </div>
+
+  <div class="scroll_container">
+    <ul>
+      {#each beers as beer}
+      <li>
+        <img src="{logo}" alt="Thecompany logo instead of beer logo">
+        <p>{beer}</p>
+        <p>Currently <span></span> on tap</p>
+        <button>V More information about {beer} V</button>
+        <p>The more information about {beer}</p>
+        <p>This beer is a type</p>
+        <p>14% <br> alchohol</p>
+        <div>
+          <span>+</span>
+          <span>0</span>
+          <span>-</span>
+        </div>
+        <p>Price: </p>
+
+      </li>
+      {/each}
+    </ul>
+  </div>
+</section>
+
+<section class="basket">
+  <div class="order">
+    <h2>Order</h2>
+    <div>
+      {#each beers as beer}
+      <div>
+        <img>
+        <div>
+          <p>{beer}</p>
+          <div>
+            <span>+</span>
+            <span>0</span>
+            <span>-</span>
+          </div>
+          <p>Price:</p>
+        </div>
+      </div>
+      {/each}
+    </div>
+    <p>Total..............</p>
+    <button>Proceed to checkout</button>
+  </div>
+
+  <Payform />
+</section>
 
   <Counter />
-
-  <p>
-    Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte
-    apps.
-  </p>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme">SvelteKit</a> for
-    the officially supported framework, also powered by Vite!
-  </p>
 </main>
 
 <style>
---
+++ src/lib/Payform.svelte
@@ -0,0 +1,31 @@
+<div class="payment">
+    <div>
+      <span>arrow</span>
+      <p>Edit Order</p>
+    </div>
+    <h2>Payment Details</h2>
+    <form>
+      <button type="button">Pay in person</button>
+      <button type="button">MobilePay</button>
+      <p>or</p>
+      <p></p>
+      <input type="text" minlength="2" name="cardHolder" id="cardHolder" placeholder="John Doe">
+      <label for="cardHolder">Card Holder</label>
+      <div>
+        <input type="text" minlength="20" maxlength="20" name="cardNumber" id="cardNumber" placeholder="XXXX XXXX XXXX XXXX">
+        <label for="cardNumber">Card Number</label>
+        <input type="text" minlength="5" maxlength="5" name="cardExp" id="cardExp" placeholder="mm/yy">
+        <label for="cardExp">Expires</label>
+        <input type="text" minlength="3" maxlength="3" name="cardCvc" id="cardCvc" placeholder='mm/yy'>
+        <label for="cardCvc">CVC</label>
+      </div>
+      <input type="text" minlength="15" name="phoneNumber" id="phoneNumber" placeholder="+45 XX XX XX XX">
+      <label for="phoneNumber">Phone Number</label>
+      <br>
+      <input type="checkbox"  id="newsletter" name="newsletter"> I would like to recieve updates and newsletters.
+      <br>
+      <input type="checkbox"  id="terms" name="terms"> I Agree to the Terms and Conditions.
+      <p>Total................</p>
+      <button type="submit"> Complete Order</button>
+    </form>
+  </div>
