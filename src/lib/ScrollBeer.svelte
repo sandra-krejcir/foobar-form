549ca96ccf363b65bccb3b186f363eb3a47879af
Sandra Krejcir <krejcir.sandra@gmail.com>
2021-12-04 13:45:05 +0100

    everything works, just need the amount update

--- src/lib/ScrollBeer.svelte
+++ src/lib/ScrollBeer.svelte
@@ -5,6 +5,62 @@
 export let beers = []
 export let readMore = false
 
+const CART = {
+  KEY: "beerbasket",
+  contents: [],
+  init() {
+    let _contents = localStorage.getItem(CART.KEY);
+    if (_contents) {
+      CART.contents = JSON.parse(_contents);
+    }
+    CART.sync();
+  },
+
+  sync() {
+    let _cart = JSON.stringify(CART.contents);
+    localStorage.setItem(CART.KEY, _cart);
+  },
+
+  add(obj) {
+    const index = CART.contents.findIndex((element) => element.name == obj.name);
+    if (index == -1) {
+      console.log(obj);
+      obj.qty = 1;
+      console.log(CART.contents);
+      CART.contents.push(obj);
+    } else {
+      CART.contents[index].qty += 1;
+    }
+
+    console.log(CART.contents);
+    this.sync();
+  },
+
+  minusOne(obj) {
+    const productQty = CART.contents.find((element) => element.name == obj.name).qty;
+
+    if (productQty > 1) {
+      const indexObj = CART.contents.find((element) => element.name == obj.name);
+      indexObj.qty--;
+      console.log(indexObj);
+      CART.update(indexObj);
+    }
+  },
+
+  update(obj) {
+    const index = CART.contents.findIndex((element) => element.name == obj.name);
+    if (obj.qty === 0) {
+      CART.contents.splice(index, 1);
+    } else {
+      CART.contents[index].qty = obj.qty;
+    }
+    CART.sync();
+  },
+};
+
+CART.init();
+
+
 
 onMount(async () => {
     const res = await fetch(`https://foobar-databar.herokuapp.com/beertypes`);
@@ -37,9 +93,16 @@ function moreInfo(event) {
         <p>This beer is a {beer.category}</p>
         <p>{beer.alc}%<br> alchohol</p>
         <div>
-          <span>+</span>
+          <span on:click= {() => {
+            CART.add({
+              name: beer.name,
+              category: beer.category,
+              price: beer.price,
+              logo: beer.logo,
+            });
+          }}>+</span>
           <span>0</span>
-          <span>-</span>
+          <span on:click= {() =>{CART.minusOne(beer)}}>-</span>
         </div>
         <p>Price: </p>
 
